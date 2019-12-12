2793d5f47d7f6cd13ba3cc4f73ca57349fe498dc
windyoffice <test@126.com>
2019-12-12 20:08:35 +0800

    添加mysql 数据配置

---
+++ src/main/java/com/windyoffice/springcachedemo/entity/User.java
@@ -0,0 +1,50 @@
+package com.windyoffice.springcachedemo.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Integer userId;
+
+    private String userName;
+
+    private String address;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", userId=").append(userId);
+        sb.append(", userName=").append(userName);
+        sb.append(", address=").append(address);
+        sb.append("]");
+        return sb.toString();
+    }
+}
